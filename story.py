c73a31dcaea5d68df45cdda56ee77cbff3928300
bear <icebear@gmx.us>
2024-08-26 22:22:52 +1200

    starting story

--- story.py
+++ story.py
@@ -1,62 +1,22 @@
 import random 
   
 # list of books is stored in the list -'books' 
-books = ['Tao Te Ching', '', ''] 
+books = ['Tao Te Ching', 'Lessons from a Living Lemuria', 'Art of War', 'Torah', 'The Secret Teaching of All Ages', 'Coraline', 'Cestui Que Vie 1666'] 
   
 # An item from the list 'books' is selected 
 # by random.choice() 
-print(random.choice(books))
+chosenBook = random.choice(books)
 
+print("A door..")
+doorChoice = input("Open - Knock - Do nothing")
 
-# Importing random module 
-import random 
-
-
-# Defining list of phrases which will help to build a story 
-Sentence_starter = ['About 100 years ago', ' In the 20 BC', 'Once upon a time'] 
-  
-character = [' there lived a king.',' there was a man named Jack.', 
-             ' there lived a farmer.'] 
-  
-time = [' One day', ' One full-moon night'] 
-  
-story_plot = [' he was passing by', ' he was going for a picnic to '] 
-  
-place = [' the mountains', ' the garden'] 
-  
-second_character = [' he saw a man', ' he saw a young lady'] 
-  
-age = [' who seemed to be in late 20s', ' who seemed very old and feeble'] 
-  
-work = [' searching something.', ' digging a well on roadside.']
-
-
-# Selecting an item from each list and concatenating them. 
-print(random.choice(Sentence_starter)+random.choice(character)+
-      random.choice(time)+random.choice(story_plot)+
-      random.choice(place)+random.choice(second_character)+
-      random.choice(age)+random.choice(work))
-
-
-
-# Importing random module 
-import random 
-  
-# Defining list of phrases which will help to build a story 
-  
-Sentence_starter = ['About 100 years ago', ' In the 20 BC', 'Once upon a time'] 
-character = [' there lived a king.',' there was a man named Jack.', 
-             ' there lived a farmer.'] 
-time = [' One day', ' One full-moon night'] 
-story_plot = [' he was passing by',' he was going for a picnic to '] 
-place = [' the mountains', ' the garden'] 
-second_character = [' he saw a man', ' he saw a young lady'] 
-age = [' who seemed to be in late 20s', ' who seemed very old and feeble'] 
-work = [' searching something.', ' digging a well on roadside.'] 
-  
-# Selecting an item from each list and concatenating them. 
-print(random.choice(Sentence_starter)+random.choice(character)+
-      random.choice(time)+random.choice(story_plot) +
-      random.choice(place)+random.choice(second_character)+
-      random.choice(age)+random.choice(work)) 
-
+if doorChoice == 'open' or doorChoice == 'Open':
+    print("It's locked")
+elif doorChoice == 'knock' or doorChoice == 'Knock':
+    print("No one answers")
+elif doorChoice == "do nothing" or doorChoice == "Do nothing":
+      print("A whispering voice behind you 'I have your book, don't forget it..'")
+      print(chosenBook + "..")
+      print("You don't remember?..")
+else:
+    print("I didn't understand that.")
