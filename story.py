c299840bbcd3d8ae512eca4310d8284a0e5568c5
bear <icebear@gmx.us>
2024-09-01 18:40:28 +1200

    loops

--- story.py
+++ story.py
@@ -1,22 +1,24 @@
-import random 
-  
-# list of books is stored in the list -'books' 
-books = ['Tao Te Ching', 'Lessons from a Living Lemuria', 'Art of War', 'Torah', 'The Secret Teaching of All Ages', 'Coraline', 'Cestui Que Vie 1666'] 
-  
-# An item from the list 'books' is selected 
-# by random.choice() 
+import random
+
+# list of books is stored in the list -'books'
+books = ['Tao Te Ching', 'Lessons from a Living Lemuria', 'Art of War', 'Torah', 'The Secret Teaching of All Ages', 'Coraline', 'Cestui Que Vie 1666']
+
+# An item from the list 'books' is selected
+# by random.choice()
 chosenBook = random.choice(books)
 
-print("A door..")
-doorChoice = input("Open - Knock - Do nothing")
+while True:
+    print("A door..")
+    doorChoice = input("Open - Knock - Do nothing").lower()
 
-if doorChoice == 'open' or doorChoice == 'Open':
-    print("It's locked")
-elif doorChoice == 'knock' or doorChoice == 'Knock':
-    print("No one answers")
-elif doorChoice == "do nothing" or doorChoice == "Do nothing":
-      print("A whispering voice behind you 'I have your book, don't forget it..'")
-      print(chosenBook + "..")
-      print("You don't remember?..")
-else:
-    print("I didn't understand that.")
+    if doorChoice == 'open':
+        print("It's locked")
+    elif doorChoice == 'knock':
+        print("No one answers")
+    elif doorChoice == "do nothing":
+        print("A whispering voice behind you 'I have your book, don't forget it..'")
+        print(chosenBook + "..")
+        print("You don't remember?..")
+        break
+    else:
+        print("I didn't understand that.")
