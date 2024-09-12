51a1b269a6a6c25a275cdf8a0d8b050e17ee87ba
bear <icebear@gmx.us>
2024-09-12 20:44:41 +1200

    pivot story to churchill

--- story.py
+++ story.py
@@ -1,26 +1,9 @@
-import random
-
-# list of books is stored in the list -'books'
-books = ['Tao Te Ching', 'Lessons from a Living Lemuria', 'Art of War', 'Torah', 'The Secret Teaching of All Ages', 'Coraline', 'Cestui Que Vie 1666']
-
-# An item from the list 'books' is selected
-# by random.choice()
-chosenBook = random.choice(books)
+print("It's a snowstorm in the tundra. You are all alone.")
 
 while True:
-    print("A door..")
-    doorChoice = input("Open - Knock - Do nothing").lower()
-
-    if doorChoice == 'open':
-        print("It's locked")
-    elif doorChoice == 'knock':
-        print("No one answers")
-    elif doorChoice == "do nothing":
-        print("A whispering voice behind you 'I have your book, don't forget it..'")
-        print(chosenBook + "..")
-        print("You don't remember?..")
+    choice = input("Press 1 to shuffle..")
+    if choice == "1":
+        print("Shuffle, shuffle")
         break
     else:
-        print("I didn't understand that.")
-        
-# nochillio 3
+        print("Try again.")
