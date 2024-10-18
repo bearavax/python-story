8fdaab3b1beb28bfe41efe795d7f7dcd6c19df94
bear <icebear@gmx.us>
2024-10-18 21:31:18 +1300

    added mining

--- story.py
+++ story.py
@@ -1,3 +1,5 @@
+import random
+
 print("It's a snowstorm in the tundra. You are all alone.")
 
 while True:
@@ -10,6 +12,18 @@ while True:
         
 print(".. ... ..")
 
-print("hello bear")
+print("hello")
 
-# peon bear
+inventory = 0
+
+while True:
+    choice = input("Press 1 to start mining or 2 to stop mining: ")
+    if choice == "1":
+        rocks = random.randint(1, 10)
+        inventory += rocks
+        print(f"You mined {rocks} rocks. Total inventory: {inventory}")
+    elif choice == "2":
+        print(f"You stopped mining. Final inventory: {inventory} rocks.")
+        break
+    else:
+        print("Invalid choice. Try again.")
